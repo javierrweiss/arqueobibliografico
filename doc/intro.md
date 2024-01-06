73b4e04d7dc33efc0a2e720d29d79d15314f413f
José Javier Blanco Rivero <javierweiss@gmail.com>
2024-01-06 14:07:08 -0300

    Commit inicial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to javierweiss/arqueobibliografico
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/javierweiss/arqueobibliografico_test.clj
@@ -0,0 +1,7 @@
+(ns javierweiss.arqueobibliografico-test
+  (:require [clojure.test :refer :all]
+            [javierweiss.arqueobibliografico :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
